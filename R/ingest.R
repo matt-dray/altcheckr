05f549283c1faabea165cce17048e011a5421675
Matt Dray <mwdray@gmail.com>
2019-08-31 22:16:07 +0100

    Add second function

--- R/ingest.R
+++ R/ingest.R
@@ -9,9 +9,33 @@ alt_get_attr <- function(url) {
   
   page_html <- read_html(url)
   
-  page_html %>% 
+  img_df <- page_html %>% 
     html_nodes("img") %>% 
     map(xml_attrs) %>% 
     map_df(~as.list(.))
   
+  return(img_df)
+  
+}
+
+#' Suggest Alt Tag Oddities
+#'
+#' @description Infer whether the alt tag is missing or strange.
+#' @param attr_df A data.frame with image attributes produced by alt_get_attr().
+#' @return A data.frame object.
+#' @export
+
+alt_check <- function(attr_df) {
+  
+  alt_df <- attr_df %>% 
+    mutate(
+      missing = ifelse(
+        is.na(alt),
+        "Missing",
+        "Not missing"
+      )
+    )
+  
+  return(alt_df)
+
 }
