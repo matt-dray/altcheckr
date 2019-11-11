36581ba5c809bd3229021281ff2433375a427bcc
matt-dray <mwdray@gmail.com>
2019-11-11 19:06:53 +0000

    Improve returned information for empty alt and spellchecking

--- R/check.R
+++ R/check.R
@@ -1,7 +1,7 @@
 #' Check for Problems with the Alt Text for Images on a Web Page
 #'
 #' @description Infer whether an image's alt tag is missing or could be improved.
-#' @param attr_df A data.frame/tibble with image attributes, produced by
+#' @param attributes_df A data.frame/tibble with image attributes, produced by
 #'     \code{altcheckr::alt_get()}.
 #' @param min_char A numeric value. If the number of characters in the alt
 #'     text is smaller than this then it's flagged as being short.
@@ -14,13 +14,11 @@
 #' @importFrom rlang .data
 #' @export
 
-alt_check <- function(attr_df, max_char = 200, min_char = 50,
-  redundant_pattern = "image of|picture of|photo of|image showing|picture showing"
-) {
+alt_check <- function(attributes_df, max_char = 200, min_char = 50, redundant_pattern = "image|picture|photo") {
   
   # Stop if not a data frame
-  if (!all(class(attr_df) %in% c("tbl_df", "tbl", "data.frame"))) {
-    stop("Input to the attr_df argument must be of class tibble or data.frame.")
+  if (!all(class(attributes_df) %in% c("tbl_df", "tbl", "data.frame"))) {
+    stop("Input to the attributes_df argument must be of class tibble or data.frame.")
   } else if (!is.numeric(min_char)|!is.numeric(max_char)) {
     stop("Input to the min_char and max_char arguments must be a numeric value.")
   } else if (!is.character(redundant_pattern)) {
@@ -28,14 +26,19 @@ alt_check <- function(attr_df, max_char = 200, min_char = 50,
   }
   
   # Check for alt issues
-  alt_df <- dplyr::mutate(attr_df,
+  alt_df <- dplyr::mutate(attributes_df,
     
-    # Check for missing alt text
-    alt_empty = ifelse(is.na(.data$alt), TRUE, FALSE),
+    # Check for empty/missing alt text
+    alt_exists = dplyr::case_when(
+      is.na(.data$alt) ~ "Missing",
+      .data$alt == "" ~ "Empty",
+      stringr::str_detect(.data$alt, "^[[:space:]]+$") ~ "Empty",
+      TRUE ~ "Exists"
+    ),
     
     # Check for short and long alt text
     char_length = dplyr::case_when(
-      is.na(.data$alt) ~ NA_character_,
+      .data$alt_exists %in% c("Missing", "Empty") ~ NA_character_,
       nchar(.data$alt) < min_char ~ "Short",
       nchar(.data$alt) > max_char ~ "Long",
       TRUE ~ "OK"
@@ -48,15 +51,23 @@ alt_check <- function(attr_df, max_char = 200, min_char = 50,
     terminal_period = ifelse(stringr::str_detect(.data$alt, "\\.$"), TRUE, FALSE),
     
     # Highlight possible incorrect spellings
-    spellcheck = ifelse(
-      length(hunspell::hunspell(.data$alt)[[1]]) > 0,
-      paste(hunspell::hunspell(.data$alt)[[1]], collapse = ", "), 
-      NA
+    spellcheck = dplyr::case_when(
+      .data$alt_exists %in% c("Empty", "Missing") ~ NA_character_,
+      is.na(.data$alt_exists) ~ NA_character_,
+      .data$alt_exists == "Exists" &
+        length(hunspell::hunspell(.data$alt)[[1]]) == 0 ~ "OK",
+      .data$alt_exists == "Exists" &
+        length(hunspell::hunspell(.data$alt)[[1]]) > 0 ~ paste(
+          "Check: ", hunspell::hunspell(.data$alt)[[1]], collapse = ", "
+        )
     ),
     
     # Give readability score
-    readability = quanteda::textstat_readability(.data$alt)$Flesch
-    
+    readability = ifelse(
+      .data$alt_exists == "Exists",
+      quanteda::textstat_readability(.data$alt)$Flesch,
+      NA_real_
+    )
   ) %>% 
     dplyr::as_tibble()  # ensure tibble output
   
