f71bcc17e194e79324bbc9c649e17f9b9017ce78
matt-dray <mwdray@gmail.com>
2019-11-11 18:03:13 +0000

    Update documentation

--- R/check.R
+++ R/check.R
@@ -1,44 +1,64 @@
-#' Check for Image Alt Tag Problems
+#' Check for Problems with the Alt Text for Images on a Web Page
 #'
 #' @description Infer whether an image's alt tag is missing or could be improved.
-#' @param attr_df A data.frame/tibble with image attributes produced by \code{altcheckr::alt_get_attr()}.
-#' @return A data.frame/tibble object.
+#' @param attr_df A data.frame/tibble with image attributes, produced by
+#'     \code{altcheckr::alt_get()}.
+#' @param min_char A numeric value. If the number of characters in the alt
+#'     text is smaller than this then it's flagged as being short.
+#' @param max_char A numeric value. If the number of characters in the alt
+#'     text is larger than this then it's flagged as being long
+#' @param redundant_pattern A character string. Regular expression for phrase(s)
+#'     to flag as being redundant in alt text ("A picture of" isn't necessary,
+#'     for example).
+#' @return A tibble object with classes "tbl_df", "tbl", "data.frame".
 #' @importFrom rlang .data
 #' @export
 
-alt_check <- function(attr_df) {
+alt_check <- function(attr_df, max_char = 200, min_char = 50,
+  redundant_pattern = "image of|picture of|photo of|image showing|picture showing"
+) {
   
   # Stop if not a data frame
-  if (!is.data.frame(attr_df)) {
-    stop("Input to the attr_df argument must be of class data.frame")
+  if (!all(class(attr_df) %in% c("tbl_df", "tbl", "data.frame"))) {
+    stop("Input to the attr_df argument must be of class tibble or data.frame.")
+  } else if (!is.numeric(min_char)|!is.numeric(max_char)) {
+    stop("Input to the min_char and max_char arguments must be a numeric value.")
+  } else if (!is.character(redundant_pattern)) {
+    stop("Input to the redundant_pattern argument must be a character string.")
   }
   
   # Check for alt issues
-  alt_df <- attr_df %>% 
-    dplyr::mutate(
-      
-      # check for missing alt text
-      missing = ifelse(is.na(.data$alt), TRUE, FALSE),
-      
-      # check for too long or short
-      length = dplyr::case_when(
-        is.na(.data$alt) ~ NA_character_,
-        nchar(.data$alt) > 200 ~ "Long",
-        nchar(.data$alt) < 50 ~ "Short",
-        TRUE ~ "OK"
-      ),
-      
-      # check for self-evident phrases
-      self_evident = ifelse(
-        stringr::str_detect(
-          tolower(.data$alt),
-          "image of|picture of|image showing|picture showing"
-        ), TRUE, FALSE
-      ),
-      
-      # check for terminal period
-      period = ifelse(stringr::str_detect(.data$alt, ".$"), TRUE, FALSE)
-    )
+  alt_df <- dplyr::mutate(attr_df,
+    
+    # Check for missing alt text
+    alt_empty = ifelse(is.na(.data$alt), TRUE, FALSE),
+    
+    # Check for short and long alt text
+    char_length = dplyr::case_when(
+      is.na(.data$alt) ~ NA_character_,
+      nchar(.data$alt) < min_char ~ "Short",
+      nchar(.data$alt) > max_char ~ "Long",
+      TRUE ~ "OK"
+    ),
+    
+    # Check for self-evident phrases
+    self_evident = ifelse(stringr::str_detect(tolower(.data$alt), tolower(redundant_pattern)), TRUE, FALSE),
+    
+    # Check for terminal punctuation
+    terminal_period = ifelse(stringr::str_detect(.data$alt, "\\.$"), TRUE, FALSE),
+    
+    # Highlight possible incorrect spellings
+    spellcheck = ifelse(
+      length(hunspell::hunspell(.data$alt)[[1]]) > 0,
+      paste(hunspell::hunspell(.data$alt)[[1]], collapse = ", "), 
+      NA
+    ),
+    
+    # Give readability score
+    readability = quanteda::textstat_readability(.data$alt)$Flesch
+    
+  ) %>% 
+    dplyr::as_tibble()  # ensure tibble output
   
   # Return
   return(alt_df)
--- R/read.R
+++ R/read.R
@@ -1,25 +1,37 @@
-#' Get a Web Page's Image Attributes
+#' Get HTML Attributes from the Image Elements of a Web Page
 #'
-#' @description Scrapes a web page and extracts the image tags into a data frame.
-#' @param url A string describing the URL of a webpage.
-#' @return A data.frame object.
+#' @description Scrape a web page's HTML and extract into a tibble the attributes
+#'     from each image element. Only collects attributes if they exist.
+#' @param url A character string describing the URL of a webpage.
+#' @param all_attributes Logical. Do you want to return all the image attributes
+#'     (\code{TRUE}), or just the \code{src} (image source) and \code{alt}
+#'     (alt text) attributes (\code{FALSE})?
+#' @return A tibble object with classes "tbl_df", "tbl", "data.frame").
 #' @export
 
-alt_get_attr <- function(url) {
+alt_get <- function(url, all_attributes = FALSE) {
   
-  # Stop if not a character string
+  # Stop for argument input problems 
   if (!is.character(url)) {
-    stop("Input to the url argument must be a character string")
+    stop("Input to the url argument must be a character string.")
+  } else if (!is.logical(all_attributes)) {
+    stop("Input to the all_attributes argument must be logical (TRUE or FALSE).")
   }
   
-  # Read the full HTML of a page
+  # Read the full HTML from the URL provided
   page_html <- xml2::read_html(url)
   
-  # Just extract the img element content
+  # Extract attributes from the img HTML elements only
   img_df <- page_html %>% 
     rvest::html_nodes("img") %>% 
     purrr::map(xml2::xml_attrs) %>% 
     purrr::map_df(~as.list(.))
+  
+  # Create an alt column if it doesn't exist
+  if (!any(names(img_df) == "alt")) img_df$alt <- NA_character_
+
+  # Keep all attribute columns or just src and alt (user specified)
+  if (all_attributes == FALSE) img_df <- img_df[, c("src", "alt")]
   
   # Return the tibble
   return(img_df)
