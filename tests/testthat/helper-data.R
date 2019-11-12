a3f34dcb4b6b0ab1722c8426baa4a913c985686b
matt-dray <mwdray@gmail.com>
2019-11-12 17:40:48 +0000

    Update basic tests

---
+++ tests/testthat/helper-data.R
@@ -0,0 +1,36 @@
+# Inputs
+
+test_vec_num <- c(1, 2, 3)
+test_vec_char <- c("a", "b", "c")
+test_vec_fct <- as.factor(test_vec_char)
+test_vec_lgl <- c(TRUE, FALSE, TRUE)
+
+test_mat <- matrix(c(1:3, c(letters[1:3]), c(TRUE, FALSE, TRUE)), 3, 3)
+test_df <- as.data.frame(test_mat)
+test_list <- as.list(test_df)
+
+test_url <- "https://rostrum.blog"
+test_url_404 <- "https://rostrum.blog/invalidtestpage"
+test_url_resolve <- "https://testrostrumdomain.blog"
+
+test_attributes_df <- dplyr::tibble(
+  src = rep("path/to/test_file.jpg", 7),
+  alt = c(
+    NA_character_,
+    "",
+    " ",
+    "Short",
+    "This has a terminal period.",
+    "This passage of text is an example of an alt text description that is very long and exceeds 125 characters in length. It could probably be made more succinct.",
+    "test_file.jpg"
+  )
+)
+
+# Outputs
+
+out_get_class <- c("tbl_df", "tbl", "data.frame")
+out_get_names <- c("src", "alt")
+out_check_names <- c(
+  "src", "alt", "alt_exists", "char_length", "self_evident", "terminal_period", 
+  "spellcheck", "readability"
+)
--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,9 +1,31 @@
 context("test-check")
 
-test_that("there's an error with non-data.frame input", {
-  expect_error(altcheckr::alt_check("a string"))  # character
-  expect_error(altcheckr::alt_check(1))  # numeric
-  expect_error(altcheckr::alt_check(TRUE))  # logical
-  expect_error(altcheckr::alt_check(bare_text))  # bare
-  expect_error(altcheckr::alt_check(iris$Species))  # vector
+test_that("there's an error with non-data.frame input to attributes_df", {
+  expect_error(altcheckr::alt_check(attribute_df = 1))
+  expect_error(altcheckr::alt_check(attribute_df = TRUE))
+  expect_error(altcheckr::alt_check(attribute_df = NA))
+  expect_error(altcheckr::alt_check(attribute_df = NULL))
+  expect_error(altcheckr::alt_check(attribute_df = test_vec_num))
+  expect_error(altcheckr::alt_check(attribute_df = test_vec_char))
+  expect_error(altcheckr::alt_check(attribute_df = test_vec_fct))
+  expect_error(altcheckr::alt_check(attribute_df = test_vec_lgl))
+  expect_error(altcheckr::alt_check(attribute_df = test_mat))
+  expect_error(altcheckr::alt_check(attribute_df = test_list))
+})
+
+test_that("there's an error with non-numeric input to min/max_char", {
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = TRUE))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = NA))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = NULL))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_vec_num))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_vec_char))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_vec_fct))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_vec_lgl))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_df))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_mat))
+  expect_error(altcheckr::alt_check(attribute_df = test_attributes_df, min_char = test_list))
+})
+
+test_that("structure of output data.frame is as expected", {
+  expect_equal(class(altcheckr::alt_check(test_attributes_df)), out_get_class)
 })
